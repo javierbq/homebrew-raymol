439bffddc3c9ade100a262f4602b49927c44d191
Javier Castellanos <javierbq@gmail.com>
2026-09-17 22:21:51 -0700

    raymol 1.11.2

--- Casks/raymol.rb
+++ Casks/raymol.rb
@@ -1,6 +1,6 @@
 cask "raymol" do
-  version "1.11.1"
-  sha256 "4fc03f6a0d95fd7ab53534a4870d9e7cbb5c1d0db1e20453d5436abc09bbad7d"
+  version "1.11.2"
+  sha256 "f41597b98ecb1e27ffd692466ca24cb5f75ebef6db93dd6f77d216475ce70094"
 
   url "https://github.com/javierbq/RayMol/releases/download/v#{version}/RayMol-#{version}.dmg"
   name "RayMol"
